b146ca59f031ad5c4cb2f991ceb3963055045446
3dpc-materialise <3dprintcloud@materialise.com>
2016-08-05 14:11:33 +0300

    examples updated

--- PHP/example.php
+++ PHP/example.php
@@ -1,4 +1,11 @@
 <?php
+/*
+This example shows basic upload-import-analyze-repair-export-download flow.
+
+If you need other operations just uncomment them and take a look on documentation 
+for the format of returning result
+*/
+
 
 namespace MaterialiseCloud;
 
@@ -12,8 +19,18 @@ require_once("OperationFileApi.php");
 require_once("OperationApiClient.php");
 
 require_once("AnalyzeOperationApiClient.php");
+require_once("WallThicknessAnalysisOperationApiClient.php");
+
 require_once("RepairOperationApiClient.php");
+require_once("GeometricalRepairOperationApiClient.php");
+require_once("ShrinkwrapRepairAccuracies.php");
+require_once("ShrinkwrapRepairOperationApiClient.php");
+
 require_once("ScaleOperationApiClient.php");
+require_once("GenerateThumbnailOperationApiClient.php");
+require_once("ReduceTrianglesOperationApiClient.php");
+require_once("HollowingOperationApiClient.php");
+
 require_once("ImportOperationApiClient.php");
 require_once("ExportOperationApiClient.php");
 require_once("TokenProviderInterface.php");
@@ -29,10 +46,8 @@ $host = "api.cloud.materialise.com"; // for production environment
 
 $tokenProvider = new TokenProvider(new AccessApiClient($host), $apiId, $apiSecret, $userEmail, $userPassword);
 
-$sourceFilePath = "meerkat_bad.3DS";
-$destinationFilePath = "meerkat_modyfied.stl";
-
 echo "uploading\n";
+$sourceFilePath = "meerkat_bad.3DS";
 $filesClient = new OperationFileApi($host, $tokenProvider);
 $fileId = $filesClient->UploadFile($sourceFilePath);
 echo "upload done\n";
@@ -62,14 +77,77 @@ if($analysisResults->BadEdges > 0
 	$repairer->WaitForOperationToFinish($operationId);
 	$resultId = $repairer->GetRepairOperationResult($operationId);
 	echo "repair done\n";	
+
+	/*
+	echo "running shrink wrap repairing\n";
+	$repairer = new ShrinkwrapRepairOperationApiClient($host, $tokenProvider);
+	$operationId = $repairer->Repair($resultId, ShrinkwrapRepairAccuracies::Rough);
+	$repairer->WaitForOperationToFinish($operationId);
+	$resultId = $repairer->GetShrinkwrapRepairOperationResult($operationId);
+	echo "repair done\n";
+	*/	
+
+	/*
+	echo "running geometrical repair\n";
+	$repairer = new GeometricalRepairOperationApiClient($host, $tokenProvider);
+	$operationId = $repairer->Repair($resultId);
+	$repairer->WaitForOperationToFinish($operationId);
+	$resultId = $repairer->GetGeometricalRepairOperationResult($operationId);
+	echo "repair done\n";
+	*/
 }
 
+/*
 echo "scaling\n";
 $scaler = new ScaleOperationApiClient($host, $tokenProvider);
 $operationId = $scaler->Scale($resultId, Axes::Y, 10);
 $scaler->WaitForOperationToFinish($operationId);
 $resultId = $scaler->GetScaleOperationResult($operationId);
 echo "scale done\n";
+*/
+
+/*echo "preview generation\n";
+$previewer = new GenerateThumbnailOperationApiClient($host, $tokenProvider);
+$operationId = $previewer->GenerateThumbnail($resultId, 300, 300, 145, 145, 145);
+$previewer->WaitForOperationToFinish($operationId);
+$fileId = $previewer->GetGenerateThumbnailOperationResult($operationId);
+echo "generation done\n";
+
+echo "download preview\n";
+$filesClient->DownloadFile($fileId,"model_preview.jpg");
+echo "download preview done\n";
+*/
+
+/*
+echo "reducing triangles\n";
+$reducer = new ReduceTrianglesOperationApiClient($host, $tokenProvider);
+$operationId = $reducer->ReduceTriangles($resultId, 2, 15, 1);
+$reducer->WaitForOperationToFinish($operationId);
+$resultId = $reducer->GetReduceTrianglesOperationResult($operationId);
+echo "reducing done\n";
+*/
+
+/**/
+echo "running hollowing\n";
+$hollower = new HollowingOperationApiClient($host, $tokenProvider);
+$operationId = $hollower->Hollow($resultId, 1);
+$hollower->WaitForOperationToFinish($operationId);
+$resultId = $hollower->GetHollowingOperationResult($operationId);
+echo "hollowing done\n";
+/**/
+
+/**/
+echo "running wallthickness analysis\n";
+$analyzer = new WallThicknessAnalysisOperationApiClient($host, $tokenProvider);
+$operationId = $analyzer->Analyze($resultId, 1, 2);
+$analyzer->WaitForOperationToFinish($operationId);
+$analysisResults = $analyzer->GetWallThicknessAnalysisOperationResult($operationId);
+echo "analysis done\n";
+
+echo "downloading analysis model\n";
+$filesClient->DownloadFile($analysisResults->FileId, "analysis_model.stl");
+echo "analysis model download done\n";
+/**/
 
 echo "exporting\n";
 $exporter = new ExportOperationApiClient($host, $tokenProvider);
@@ -79,7 +157,8 @@ $exportedFileId = $exporter->GetExportOperationResult($operationId);
 echo "export done\n";
 
 echo "download\n";
-$filesClient->DownloadFile($exportedFileId,$destinationFilePath);
+$destinationFilePath = "meerkat_modified.stl";
+$filesClient->DownloadFile($exportedFileId, $destinationFilePath);
 echo "download done\n";
 
 ?>
